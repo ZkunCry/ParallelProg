52152a45fec5e91d2adc5326f2140d393a3ca77d
Eugene Aksentev <aksentev.03@mail.ru>
2025-02-19 22:27:29 +0500

    finished lab2

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -6,52 +6,50 @@
 
 int a[N][N];
 
-#define SCHED_OPT static
- //#define SCHED_OPT dynamic
+// #define SCHED_OPT static
+// #define SCHED_OPT dynamic
 // #define SCHED_OPT guided
-//#define SCHED_OPT auto
- //#define SCHED_OPT runtime
+#define SCHED_OPT auto
+// #define SCHED_OPT runtime
 
 int main()
 {
-    int i, j;
-    double start;
 
-    // Последовательная версия
-    start = omp_get_wtime();
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            a[i][j] = rand();
-    printf("sync:        %02.3lf s.\n", omp_get_wtime() - start);
+	int i, j, k = 1;
+	double start;
+	start = omp_get_wtime();
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			a[i][j] = rand();
 
-    // Параллельная версия с внутренним циклом
-    start = omp_get_wtime();
-    for (i = 0; i < N; i++) {
-        #pragma omp parallel for schedule(SCHED_OPT)
-        for (j = 0; j < N; j++)
-            a[i][j] = rand();
-       
-    }
-    printf("internal:    %02.3lf s.\n", omp_get_wtime() - start);
+	printf("sync:		%02.3lf s.\n", omp_get_wtime() - start);
+	start = omp_get_wtime();
+	for (i = 0; i < N; i++) {
+         #pragma omp parallel for schedule(SCHED_OPT)
+					for (j = 0; j < N; j++)
+						a[i][j] = rand();
+	
+	}
 
-    // Параллельная версия с внешним циклом
-    start = omp_get_wtime();
-#pragma omp parallel for private(j) schedule(SCHED_OPT)
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            a[i][j] = rand();
-    printf("external:    %02.3lf s.\n", omp_get_wtime() - start);
+	printf("internal:	%02.3lf s.\n", omp_get_wtime() - start);
+	start = omp_get_wtime();
+   #pragma omp parallel for private(j) schedule(SCHED_OPT)
+		for (i = 0; i < N; i++)
+			for (j = 0; j < N; j++)
+				a[i][j] = rand();
 
-    // Параллельная версия с collapse
-    start = omp_get_wtime();
-#pragma omp parallel
-    {
-#pragma omp for schedule(SCHED_OPT)
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                a[i][j] = rand();
-    }
-    printf("collapse:    %02.3lf s.\n", omp_get_wtime() - start);
+	printf("external:	%02.3lf s.\n", omp_get_wtime() - start);
+	start = omp_get_wtime();
 
-    return 0;
+    #pragma omp parallel
+	{
+    #pragma omp for collapse(2) schedule(SCHED_OPT)
+		for (i = 0; i < N; i++)
+			for (j = 0; j < N; j++)
+				a[i][j] = rand();
+	}
+
+	printf("collapse:	%02.3lf s.\n", omp_get_wtime() - start);
+
+	return 0;
 }
